57b779fddeed476ba43422614142cd5ed6e12ff3
Brandol Fontecha <brandol.fontecha@moviired.co>
2023-09-03 22:16:35 -0500

    Inicializando proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: ['comfortaa_regular', 'comfortaa-light', 'comfortaa-bold', 'comfortaa-semibold', 'system-ui']
+    },
+  },
+  plugins: [],
+}
+
