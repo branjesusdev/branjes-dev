02b5271950329c05494e72135658f6b599670d94
Brandol Fontecha <brandol.fontecha@moviired.co>
2023-09-05 00:21:34 -0500

    Add extensions and Design overview section profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: ['comfortaa_regular', 'comfortaa-light', 'comfortaa-bold', 'comfortaa-semibold', 'system-ui']
+      fontFamily: ['comfortaa-regular', 'comfortaa-light', 'comfortaa-bold', 'comfortaa-semibold', 'system-ui'],
+      colors: {
+        blue: {
+          magneta: 'var(--blue-magneta)'
+        },
+        green :{
+          magneta: 'var(--green-magneta)'
+        },
+        brav: {
+          primary: '#cced89',
+          secondary: '#7cba01',
+        }
+      }
     },
   },
   plugins: [],
