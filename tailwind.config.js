9b55f042e063b5a88e761fc3deb123bb1139e9d1
Brandol Fontecha <brandol.fontecha@moviired.co>
2023-09-14 22:19:11 -0500

    Efects images projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,20 @@ export default {
           primary: '#cced89',
           secondary: '#7cba01',
         }
+      },
+
+      animation: {
+        movedeg: 'movedeg .3s ease-in 1 forwards',
+      },
+
+      keyframes: {
+        movedeg: {
+          '0%': { transform: 'rotate(-2deg) translate(1rem, 0)' },
+          '30%': { transform: 'rotate(-5deg) translate(2rem, 0)' },
+          '50%': { transform: 'rotate(-8deg) translate(3rem, 0)' },
+          '80%': { transform: 'rotate(-10deg) translate(4rem, 0)' },
+          '100%': { transform: 'rotate(-12deg) translate(5rem, 0)' }
+        }
       }
     },
   },
