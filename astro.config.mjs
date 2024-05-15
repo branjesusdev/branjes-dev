722e2337f08ee6e1d14439d20e38e689a014635b
Brandol Vargas <brandol.fontecha@moviired.co>
2024-05-15 01:08:42 -0500

    Update Astro to 4.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  experimental: {
+    security: {
+      csrfProtection: {
+        origin: true,
+      },
+    },
+  },
   integrations: [tailwind()],
 });
