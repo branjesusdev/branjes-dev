370f904820fff2c775310beef105e335eaf3c291
Brandol Vargas <brandol.fontecha@moviired.co>
2024-08-13 21:40:33 -0500

    Update repository + location + desing + 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,59 +1,55 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from "@astrojs/sitemap"
-import { VitePWA } from "vite-plugin-pwa"
+import sitemap from "@astrojs/sitemap";
+import { VitePWA } from "vite-plugin-pwa";
 
 // Helper imports
-import { manifest, seoConfig } from "./src/utils/seoConfig"
+import { manifest, seoConfig } from "./src/utils/seoConfig";
 
+// https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
   compressHTML: true,
-	prefetch: true,
-  integrations: [tailwind(),sitemap()],
+  prefetch: true,
+  integrations: [tailwind(), sitemap()],
   site: seoConfig.baseURL,
   vite: {
     vite: {
       build: {
-        cssMinify: "lightningcss",
+        cssMinify: "lightningcss"
       },
-      plugins: [
-        VitePWA({
-          registerType: "autoUpdate",
-          manifest,
-          workbox: {
-            globDirectory: ".vercel/output/static",
-            globPatterns: ["**/*.{html,js,css,woff,woff2,ttf,eot,ico}"],
-            runtimeCaching: [
-              {
-                urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
-                handler: "CacheFirst",
-                options: {
-                  cacheName: "images",
-                  expiration: {
-                    maxEntries: 100,
-                    maxAgeSeconds: 30 * 24 * 60 * 60,
-                  },
-                },
-              },
-              {
-                urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
-                handler: "CacheFirst",
-                options: {
-                  cacheName: "fonts",
-                  expiration: {
-                    maxEntries: 10,
-                    maxAgeSeconds: 30 * 24 * 60 * 60,
-                  },
-                },
-              },
-            ],
-            navigateFallback: null,
-          },
-        }),
-      ],
+      plugins: [VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        workbox: {
+          globDirectory: ".vercel/output/static",
+          globPatterns: ["**/*.{html,js,css,woff,woff2,ttf,eot,ico}"],
+          runtimeCaching: [{
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+              }
+            }
+          }, {
+            urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "fonts",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+              }
+            }
+          }],
+          navigateFallback: null
+        }
+      })]
     }
   }
 });
